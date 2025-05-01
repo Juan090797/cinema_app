6225e971771428fa83c952f728c5a2aa7ab55d2d
Juan  Marquina <juanantoniomarquina@gmail.com>
2025-05-01 00:05:44 -0500

    feat: add lista horizontal

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:cinema_app/presentation/providers/movies/movies_providers.dart';
+import 'package:cinema_app/presentation/providers/providers.dart';
 import 'package:cinema_app/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,7 +10,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: _HomeView());
+    return const Scaffold(
+      body: _HomeView(),
+      bottomNavigationBar: CustomBottomNavigation(),
+      );
   }
 }
 
@@ -31,16 +34,20 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
-    //if( nowPlayingMovies.length == 0) return CircularProgressIndicator();
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
 
     return Column(
       children: [
-
-        CustomAppbar(),
-
-       MoviesSlideShow(movies:  nowPlayingMovies),
+        const CustomAppbar(),
+        MoviesSlideShow(movies: slideShowMovies),
+        
+        MoviesHorizontalListview(
+          movies: nowPlayingMovies,
+          title: 'En cines',
+          subTitle: 'Lunes 20',
+        )
       ],
     );
   }
